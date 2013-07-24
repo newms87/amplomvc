9609abab293e92b7c7e42fabfd5254613d0d3aa0
newms87 <newms87@gmail.com>
2013-07-24 17:30:56 -0500

    Updating getTemplatesFrom to allow admin templates as well.

--- admin/model/design/template.php
+++ admin/model/design/template.php
@@ -1,19 +1,22 @@
 <?php
 class Admin_Model_Design_Template extends Model
 {
-	public function getTemplatesFor($view, $with_blanks = false)
+	public function getTemplatesFrom($path, $admin = false)
 	{
-		$themes = scandir(SITE_DIR . 'catalog/view/theme/');
-		
-		if (!$themes) {
-			trigger_error("There was a problem reading from the catalog theme directory!");
-			exit;
+		if ($admin) {
+			$root = SITE_DIR . 'admin/';
+			$themes = $this->theme->getAdminThemes();
+		}
+		else {
+			$root = SITE_DIR . 'catalog/view/theme/';
+			$themes = $this->theme->getThemes();
 		}
 		
 		$templates = array();
 	
 		foreach ($themes as $theme) {
-			$dir = SITE_DIR . 'catalog/view/theme/' . $theme . '/template/' . $view . '/';
+			$dir = $root . $theme . '/template/' . trim($path,'/') . '/';
+			
 			if (!is_dir($dir)) {
 				continue;
 			}
@@ -22,15 +25,11 @@ class Admin_Model_Design_Template extends Model
 			
 			$template_files = array();
 			
-			foreach ($files as $key => $file) {
-				if (!in_array($file, array('.','..')) && is_file($dir. $file) && preg_match("/\.tpl$/",$file) > 0) {
-					$file = str_replace('.tpl', '', $file);
-					$template_files[$file] = $file;
+			foreach ($files as $file) {
+				if (is_file($dir . $file) && preg_match("/\.tpl$/", $file) > 0) {
+					$filename = str_replace('.tpl', '', $file);
+					$template_files[$filename] = $filename;
 				}
-			}
-			
-			if ($with_blanks) {
-				$template_files = array('' => '') + $template_files;
 			}
 			
 			$templates[$theme] = $template_files;
