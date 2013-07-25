822907577451ab16195c0236df6c7800eab6a075
newms87 <newms87@gmail.com>
2013-07-25 00:21:41 -0500

    Added Language File mod for collections

---
+++ plugin/collections/file_mods/catalog/language/english/product/category.php
@@ -0,0 +1,11 @@
+#<?php
+//<<<<<
+$_['text_all_categories'] = "All Categories";
+$_['text_title_all'] = "All Categories";
+$_['text_name_all'] = "All Categories";
+//-----
+//>>>>>
+$_['text_all_categories'] = "All Collections";
+$_['text_title_all'] = "All Collections";
+$_['text_name_all'] = "All Collections";
+//-----
