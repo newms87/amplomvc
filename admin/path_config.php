e18b01067ba49ef3afa4e8dc4c4d188f890e55d6
newms87 <newms87@gmail.com>
2013-08-01 22:00:10 -0500

    Adding Path configs to Version controlled files.

--- admin/path_config.php
+++ admin/path_config.php
@@ -30,3 +30,17 @@ define('DIR_THEME', SITE_DIR . 'admin/view/theme/');
 define('DIR_THEME_OPTION', SITE_DIR . 'admin/view/template_option/');
 define('DIR_CATALOG', SITE_DIR . 'catalog/');
 define('DIR_GENERATED_IMAGE', SITE_DIR . 'image/generated/');
+
+//SYSTEM DIRECTORIES
+define('DIR_EXCEL_TEMPLATE', SITE_DIR . 'system/php-excel/templates/');
+define('DIR_EXCEL_FPO', SITE_DIR . 'upload/fpo/');
+define('DIR_SYSTEM', SITE_DIR . 'system/');
+define('DIR_DATABASE', SITE_DIR . 'system/database/');
+define('DIR_DATABASE_BACKUP', SITE_DIR . 'system/database/backups/');
+define('DIR_CONFIG', SITE_DIR . 'system/config/');
+define('DIR_IMAGE', SITE_DIR . 'image/');
+define('DIR_JS', SITE_DIR . 'system/javascript/');
+define('DIR_CACHE', SITE_DIR . 'system/cache/');
+define('DIR_DOWNLOAD', SITE_DIR . 'download/');
+define('DIR_MERGED_FILES', SITE_DIR . 'system/plugins/merged/');
+define('DIR_LOGS', SITE_DIR . 'system/logs/');
