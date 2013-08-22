819c36dc5e373b018204e835e9ec3a5717817aa0
newms87 <newms87@gmail.com>
2013-08-22 18:49:31 -0500

    Fixed User Install (for reals!)

--- system/install/install.php
+++ system/install/install.php
@@ -5,6 +5,9 @@ if (!defined("AMPLOCART_INSTALL")) {
 	exit;
 }
 
+define("SITE_DIR", str_replace('system/install','', rtrim(str_replace('\\','/',dirname(__FILE__)), '/')));
+define("DIR_DATABASE", SITE_DIR . 'system/database/');
+
 $language = (!empty($_GET['language']) && is_file($_GET['language'] . '.php')) ? $_GET['language'] : 'english';
 
 $_ = array();
@@ -94,8 +97,6 @@ require_once("system/install/install_{$template}.tpl");
 
 
 function setup_db($_) {
-	define("SITE_DIR", str_replace('system/install','', rtrim(str_replace('\\','/',dirname(__FILE__)), '/')));
-	define("DIR_DATABASE", SITE_DIR . 'system/database/');
 	define("DB_PREFIX", $_POST['db_prefix']);
 
 	require_once(DIR_DATABASE . "database.php");
@@ -168,6 +169,9 @@ function setup_db($_) {
 
 	$contents = preg_replace($patterns, $replacements, $contents);
 
+	//Allows for user installation (will be removed after user installation
+	$contents .= "\r\n\r\ndefine(\"AMPLOCART_INSTALL_USER\", 1);";
+
 	file_put_contents($oc_config, $contents);
 
 	//Setup .htaccess file
@@ -212,7 +216,7 @@ function setup_user($_) {
 	//remove user install configuration
 	$contents = file_get_contents($oc_config);
 
-	$contents = str_replace("define(\"AMPLOCART_INSTALL_USER\", 1);", '', $contents);
+	$contents = str_replace("\r\n\r\ndefine(\"AMPLOCART_INSTALL_USER\", 1);", '', $contents);
 
 	file_put_contents($oc_config, $contents);
 
