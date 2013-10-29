a5b129c601e6f7bc2fc5b34494b53fe0e7dc82ed
newms87 <newms87@gmail.com>
2013-10-29 15:11:13 -0600

    * changing ac_cron.php to live version

--- system/cron/ac_cron.php
+++ system/cron/ac_cron.php
@@ -6,7 +6,5 @@ class System_Cron_AcCron extends CronJob
 		$backup_file = DIR_DATABASE_BACKUP . 'scheduled_backup-' . $this->date->format(null, 'y-m-d') . '.sql';
 
 		$this->db->dump($backup_file);
-
-		echo'backuped if its 4:05pm then this is working! come delete me! ' . __METHOD__;exit;
 	}
 }
