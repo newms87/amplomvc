1adb1f82fe731d83646cf1540da71b87b9b7cae1
newms87 <newms87@gmail.com>
2013-11-15 16:57:14 -0700

    Fixed bug in system updater (using natsort instead of alphanumeric sort)

--- system/update.php
+++ system/update.php
@@ -1,31 +1,33 @@
 <?php
 class System_Update {
 	private $registry;
-	
+
 	public function __construct($registry)
 	{
 		$this->registry = $registry;
 	}
-	
+
 	public function __get($key)
 	{
 		return $this->registry->get($key);
 	}
-	
+
 	public function getVersions()
 	{
 		$version_list = $this->tool->get_files_r(DIR_SYSTEM . 'updates/', array('php'), FILELIST_STRING);
-		
+
+		natsort($version_list);
+
 		$versions = array();
-		
+
 		foreach ($version_list as $version) {
 			$v = str_replace('.php','',basename($version));
 			$versions[$v] = $version;
 		}
-		
+
 		return $versions;
 	}
-	
+
 	/*
 	 * Update the database and run any necessary configurations based
 	 * on the current file version of the system
@@ -35,15 +37,15 @@ class System_Update {
 		if (!$update_version) {
 			$update_version = AC_VERSION;
 		}
-		
+
 		$versions = $this->getVersions();
-		
+
 		foreach ($versions as $version => $file) {
 			if (version_compare($update_version, $version, '>=')) {
 				require_once($file);
 			}
 		}
-		
+
 		$this->config->save('system', 'ac_version', AC_VERSION, 0);
 	}
 }
