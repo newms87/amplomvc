749eb5e47b097fdbd3168343d89a12b61f40aa82
newms87 <newms87@gmail.com>
2014-01-09 23:08:00 -0700

    Adding forgotten email for admin user

---
+++ catalog/controller/mail/forgotten_admin.php
@@ -0,0 +1,24 @@
+<?php
+class Catalog_Controller_Mail_ForgottenAdmin extends Controller
+{
+	public function index(array $data)
+	{
+		$this->mail->init();
+
+		$this->mail->setTo($data['email']);
+		$this->mail->setFrom($this->config->get('config_email'));
+		$this->mail->setSender($this->config->get('config_name'));
+		$this->mail->setSubject(_l("Password Reset for %s", $this->config->get('config_name')));
+
+		//Template Data
+		$this->data += $data;
+
+		$this->data['store_name'] = $this->config->get('config_name');
+
+		//Render Mail Template
+		$this->template->load('mail/forgotten_admin');
+		$this->mail->setHtml($this->render());
+
+		$this->mail->send();
+	}
+}
