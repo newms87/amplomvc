f6b1ca801d3205f045a9b9080e6beb912385d69a
newms87 <newms87@gmail.com>
2014-01-27 22:01:34 -0700

    Moved Subscriptions in management.php to plugin

--- catalog/controller/account/management.php
+++ catalog/controller/account/management.php
@@ -31,48 +31,6 @@ class Catalog_Controller_Account_Management extends Controller
 
 		$this->data['customer'] = $customer;
 
-		//Subscriptions
-		$filter = array(
-			'statuses' => array(
-				Subscription::ACTIVE,
-				Subscription::ON_HOLD,
-			)
-		);
-
-		$subscriptions = $this->subscription->getCustomerSubscriptionsForCustomer($filter);
-
-		if ($this->subscription->hasError()) {
-			$this->message->add('warning', $this->subscription->getError());
-		}
-
-		$thumb_width  = $this->config->get('config_image_category_width');
-		$thumb_height = $this->config->get('config_image_category_height');
-
-		foreach ($subscriptions as $key => &$subscription) {
-			switch ($subscription['status']) {
-				case Subscription::ACTIVE:
-					$subscription['edit'] = $this->url->link('account/subscription', 'subscription_id=' . $subscription['customer_subscription_id']);
-					break;
-
-				case Subscription::ON_HOLD:
-					$subscription['edit']        = $this->url->link('account/subscription', 'subscription_id=' . $subscription['customer_subscription_id']);
-					$subscription['resume']      = $this->url->link('account/subscription/resume', 'subscription_id=' . $subscription['customer_subscription_id']);
-					$subscription['resume_date'] = $this->date->format($this->subscription->getMeta($subscription['customer_subscription_id'], 'resume_date'), 'medium');
-					break;
-
-				default:
-					unset($subscriptions[$key]);
-					break;
-			}
-
-			$subscription['status_class'] = $this->tool->getSlug($subscription['status']);
-
-			$subscription['thumb'] = $this->image->resize($subscription['product']['image'], $thumb_width, $thumb_height);
-		}
-		unset($subscription);
-
-		$this->data['data_subscriptions'] = $subscriptions;
-
 		//Urls
 		$this->data['url_order_history'] = $this->url->link('account/order');
 		$this->data['url_returns']       = $this->url->link('account/return');
