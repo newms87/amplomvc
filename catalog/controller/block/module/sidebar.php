eea1c4c26489939f8a49101966e9f7c16607d00d
newms87 <newms87@gmail.com>
2014-02-08 00:02:32 -0700

    Fixed bug in sidebar module

--- catalog/controller/block/module/sidebar.php
+++ catalog/controller/block/module/sidebar.php
@@ -27,51 +27,53 @@ class Catalog_Controller_Block_Module_Sidebar extends Controller
 
 			$url_query = $this->url->getQuery('category_id');
 
-			foreach ($settings['attributes'] as $attribute_menu) {
-				$attribute_group_id = $attribute_menu['attribute_group_id'];
+			if (!empty($settings['attributes'])) {
+				foreach ($settings['attributes'] as $attribute_menu) {
+					$attribute_group_id = $attribute_menu['attribute_group_id'];
 
-				$sort = array(
-					'sort'  => 'name',
-					'order' => 'ASC',
-				);
+					$sort = array(
+						'sort'  => 'name',
+						'order' => 'ASC',
+					);
 
-				$filter = array(
-					'category_ids'        => array($category_id),
-					'attribute_group_ids' => array($attribute_group_id),
-				);
+					$filter = array(
+						'category_ids'        => array($category_id),
+						'attribute_group_ids' => array($attribute_group_id),
+					);
 
-				$attribute_list = $this->Model_Catalog_Product->getAttributes($sort + $filter);
+					$attribute_list = $this->Model_Catalog_Product->getAttributes($sort + $filter);
 
-				if (empty($attribute_list)) {
-					continue;
+					if (empty($attribute_list)) {
+						continue;
+					}
+
+					//Setup attribute menu items
+					foreach ($attribute_list as &$attribute) {
+						//Build Attribute Query
+						$attribute_filter                      = $current_filter;
+						$attribute_filter[$attribute_group_id] = $attribute['attribute_id'];
+						$attribute_query                       = http_build_query(array('attribute' => $attribute_filter));
+
+						$attribute['href'] = $this->url->link($route, $url_query . '&' . $attribute_query);
+					}
+					unset($attribute);
+
+					//Remove Filter for this attribute for the All menu link
+					$attribute_filter = $current_filter;
+					unset($attribute_filter[$attribute_group_id]);
+					$attribute_query = http_build_query(array('attribute' => $attribute_filter));
+
+					$menu = array(
+						'name'     => $attribute_menu['group_name'],
+						'href'     => $this->url->link($route, $url_query . '&' . $attribute_query),
+						'children' => $attribute_list,
+					);
+
+					$this->data['attribute_menu'][] = array(
+						'label' => $attribute_menu['menu_name'],
+						'menu'  => array($menu)
+					);
 				}
-
-				//Setup attribute menu items
-				foreach ($attribute_list as &$attribute) {
-					//Build Attribute Query
-					$attribute_filter                      = $current_filter;
-					$attribute_filter[$attribute_group_id] = $attribute['attribute_id'];
-					$attribute_query                       = http_build_query(array('attribute' => $attribute_filter));
-
-					$attribute['href'] = $this->url->link($route, $url_query . '&' . $attribute_query);
-				}
-				unset($attribute);
-
-				//Remove Filter for this attribute for the All menu link
-				$attribute_filter = $current_filter;
-				unset($attribute_filter[$attribute_group_id]);
-				$attribute_query = http_build_query(array('attribute' => $attribute_filter));
-
-				$menu = array(
-					'name'     => $attribute_menu['group_name'],
-					'href'     => $this->url->link($route, $url_query . '&' . $attribute_query),
-					'children' => $attribute_list,
-				);
-
-				$this->data['attribute_menu'][] = array(
-					'label' => $attribute_menu['menu_name'],
-					'menu'  => array($menu)
-				);
 			}
 		}
 
