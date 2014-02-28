28f7d87a467fc79598927712e3dd212661ca861e
newms87 <newms87@gmail.com>
2014-02-27 22:19:47 -0700

    Adding Carousel slidesjs (hardcoded)

--- catalog/controller/block/widget/carousel.php
+++ catalog/controller/block/widget/carousel.php
@@ -3,8 +3,6 @@ class Catalog_Controller_Block_Widget_Carousel extends Controller
 {
 	public function index($settings, $carousel_id = null)
 	{
-		//Template and Language
-		$this->template->load('block/widget/carousel');
 		if (!is_null($carousel_id)) {
 			$settings = $this->Model_Block_Block->getBlockProfileSettings('widget/carousel', $carousel_id) + $settings;
 		}
@@ -22,30 +20,76 @@ class Catalog_Controller_Block_Widget_Carousel extends Controller
 		}
 		unset($slide);
 
+		$settings['slider'] = 'slidejs';
+
 		//Params
-		$default_params = array(
-			'effect'           => 'random',
-			'slices'           => 22,
-			'boxCols'          => 12,
-			'boxRows'          => 6,
-			'animSpeed'        => 500,
-			'pauseTime'        => 4000,
-			'startSlide'       => 0,
-			'directionNav'     => false,
-			'controlNav'       => false,
-			'controlNavThumbs' => false,
-			'pauseOnHover'     => false,
-			'manualAdvance'    => false,
-			'prevText'         => 'Prev',
-			'nextText'         => 'Next',
-			'randomStart'      => false,
-		);
+		switch ($settings['slider']) {
+			case 'nivo':
+			$default_params = array(
+				'effect'           => 'random',
+				'slices'           => 22,
+				'boxCols'          => 12,
+				'boxRows'          => 6,
+				'animSpeed'        => 500,
+				'pauseTime'        => 4000,
+				'startSlide'       => 0,
+				'directionNav'     => false,
+				'controlNav'       => false,
+				'controlNavThumbs' => false,
+				'pauseOnHover'     => false,
+				'manualAdvance'    => false,
+				'prevText'         => 'Prev',
+				'nextText'         => 'Next',
+				'randomStart'      => false,
+			);
+
+			break;
+
+			case 'slidejs':
+			default:
+				$default_params = array(
+					'width' => 1024,
+				   'height' => 400,
+				   'start' => 1,
+				   'navigation' => array(
+					   'active' => false,
+				      'effect' => 'fade',
+				   ),
+				   'pagination' => array(
+					   'active' => false,
+				      'effect' => 'fade',
+				   ),
+				   'play' => array(
+					   'active' => false,
+				      'effect' => 'fade',
+				      'interval' => 5000,
+				      'auto' => true,
+				      'swap' => false,
+				      'pauseOnHover' => true,
+				      'restartDelay' => 2500,
+				   ),
+				   'effect' => array(
+					   'slide' => array(
+						   'speed' => 200,
+					   ),
+					   'fade' => array(
+						   'speed' => 300,
+						   'crossfade' => true,
+					   ),
+				   ),
+				);
+
+				break;
+		}
 
 		$settings['params'] += $default_params;
 
 		$this->data['json_params'] = json_encode($settings['params']);
 
 		$this->data += $settings;
+
+		//The Template
+		$this->template->load('block/widget/carousel');
 
 		//Render
 		$this->render();
--- catalog/view/theme/default/template/block/widget/carousel.tpl
+++ catalog/view/theme/default/template/block/widget/carousel.tpl
@@ -1,14 +1,14 @@
-<div id="block_widget_carousel" class="box">
+<div class="block_widget_carousel" class="box">
 
 	<? if (!empty($show_block_title)) { ?>
 		<div class="box_heading"><?= _l("AC Carousel"); ?></div>
 	<? } ?>
 
 	<div class="box_content">
-		<div id="<?= $slider_id; ?>" <?= count($slides) > 1 ? "class=\"nivoSlider\"" : ''; ?>>
+		<div id="<?= $slider_id; ?>" <?= count($slides) > 1 ? "class=\"slider $slider\"" : ''; ?>>
 			<? foreach ($slides as $slide) { ?>
 				<? if (!empty($slide['thumb'])) { ?>
-					<img src="<?= $slide['thumb']; ?>"/>
+					<img class="slide" src="<?= $slide['thumb']; ?>"/>
 				<? } ?>
 			<? } ?>
 		</div>
@@ -16,7 +16,17 @@
 </div>
 
 <? if (count($slides) > 1) { ?>
-	<script type="text/javascript">
-		$('#<?= $slider_id; ?>').nivoSlider(<?= $json_params; ?>);
-	</script>
+	<? if ($slider === 'nivo') { ?>
+		<script type="text/javascript" src="<?= HTTP_JS . 'jquery/nivo_slider/nivo-slider.js'; ?>"></script>
+
+		<script type="text/javascript">
+			$('#<?= $slider_id; ?>').nivoSlider(<?= $json_params; ?>);
+		</script>
+	<? } else { ?>
+		<script type="text/javascript" src="<?= HTTP_JS . 'jquery/slidejs/jquery.slides.min.js'; ?>"></script>
+
+		<script type="text/javascript">
+			$('#<?= $slider_id; ?>').slidesjs(<?= $json_params; ?>);
+		</script>
+	<? } ?>
 <? } ?>
