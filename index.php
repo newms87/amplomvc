c975e92f2eeb57d6dd20c58f35dcf20d9b2191fa
Dan <dnewman@realgreenmarketing.com>
2014-08-01 12:53:29 -0600

    a

--- index.php
+++ index.php
@@ -4,10 +4,12 @@ if (is_file('ac_config.php')) {
 	include_once('ac_config.php');
 }
 
+//Default server values in case they are not set.
 $_SERVER += array(
 	'HTTP_HOST'      => DOMAIN,
 	'REQUEST_METHOD' => 'GET',
 	'REMOTE_ADDR'    => '::1',
+	'QUERY_STRING'   => '',
 );
 
 // Install
