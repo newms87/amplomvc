4b5b7667a0e6fce10ed1e45c19fe9d967fdbc120
Dan <dnewman@realgreenmarketing.com>
2015-01-16 09:45:20 -0700

    a

--- app/model/log.php
+++ app/model/log.php
@@ -2,4 +2,13 @@
 class App_Model_Log extends App_Model_Table
 {
 	protected $table = 'log', $primary_key = 'log_id';
+
+	public function getColumns($filter = array())
+	{
+		$columns = parent::getColumns($filter);
+
+		$columns['message']['align'] = 'left';
+
+		return $columns;
+	}
 }
