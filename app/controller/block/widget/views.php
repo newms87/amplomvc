ce2953fd25467a123d5f41d8e09b7d211d7e56d1
Dan <dnewman@realgreenmarketing.com>
2015-02-10 09:27:55 -0700

    * fixed bug in views

--- app/controller/block/widget/views.php
+++ app/controller/block/widget/views.php
@@ -278,7 +278,7 @@ class App_Controller_Block_Widget_Views extends App_Controller_Block_Block
 
 	public function save_view()
 	{
-		$view_id = $this->Model_View->save(_post(['view_id']), $_POST);
+		$view_id = $this->Model_View->save(_post('view_id'), $_POST);
 
 		if ($view_id) {
 			message('success', _l("%s view was saved!", _post('title')));
@@ -290,7 +290,7 @@ class App_Controller_Block_Widget_Views extends App_Controller_Block_Block
 			message('view_id', $view_id);
 			output_message();
 		} else {
-			redirect(_post('path'));
+			redirect(_post('path',' admin'));
 		}
 	}
 
