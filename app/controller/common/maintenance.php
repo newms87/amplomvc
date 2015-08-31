47694ba164a671909e4a8d0fc7d363385b1a9469
newms87 <newms87@gmail.com>
2015-08-30 21:51:57 -0600

    a

--- app/controller/common/maintenance.php
+++ app/controller/common/maintenance.php
@@ -7,6 +7,8 @@ class App_Controller_Common_Maintenance extends Controller
 		//Page Head
 		set_page_info('title', _l("Maintenance"));
 
+		$this->document->setLinks('primary', array());
+		
 		//Render
 		output($this->render('common/maintenance'));
 	}
