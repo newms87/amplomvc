3cb5e5c50a2c7e5aab31592ba8189c2dd8a80224
Daniel Newman <newms87@gmail.com>
2015-11-19 23:38:33 -0700

    * date secondsToTime() method

--- system/library/date.php
+++ system/library/date.php
@@ -23,18 +23,18 @@ class Date extends Library
 	);
 
 	static $months_abbrev = array(
-			1  => 'Jan',
-			2  => 'Feb',
-			3  => 'Mar',
-			4  => 'Apr',
-			5  => 'May',
-			6  => 'Jun',
-			7  => 'Jul',
-			8  => 'Aug',
-			9  => 'Sept',
-			10 => 'Oct',
-			11 => 'Nov',
-			12 => 'Dec',
+		1  => 'Jan',
+		2  => 'Feb',
+		3  => 'Mar',
+		4  => 'Apr',
+		5  => 'May',
+		6  => 'Jun',
+		7  => 'Jul',
+		8  => 'Aug',
+		9  => 'Sept',
+		10 => 'Oct',
+		11 => 'Nov',
+		12 => 'Dec',
 	);
 
 	private $timezone;
@@ -346,5 +346,10 @@ class Date extends Library
 		}
 
 		return $date->format($format);
+	}
+
+	public function secondsToTime($seconds)
+	{
+		return _l("%02d:%02d:%02d", $seconds / 3600, $seconds / 60 % 60, $seconds % 60);
 	}
 }
